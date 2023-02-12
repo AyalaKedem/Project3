ff6a71553165606722944cd379d490c78c3bfcc7
AyalaKedem <ayala.bl28@gmail.com>
2023-02-12 02:00:53 +0200

    add paragraph in about

--- src/routes/about/About.tsx
+++ src/routes/about/About.tsx
@@ -1,18 +1,21 @@
 import { v4 } from 'uuid';
 import aboutImg from '../../img/jason-leung-poI7DelFiVA-unsplash.jpg';
 
-import { about } from '../../services/about';
+import { about, moreAbout } from '../../services/about';
 import css from './About.module.scss'
 
 const About = () => {
   return (
     <>
-    <div className='w-75 mx-auto d-flex flex-column flex-xl-row justify-content-between gap-5 p-5'>
+    <div className='w-75 mx-auto p-5 mt-1'>
+    <div className='d-flex flex-column flex-xl-row justify-content-between gap-5'>
       <div>
       <h2 className={`display-2 text-end ${css.orange}`}>אודותנו</h2>
       <div className='text-end'>{about.map(p => (<p key={v4()}>{p}</p>))}</div>
       </div>
-      <img className='w-50' src={aboutImg} alt="תמונת המסעדה מבחוץ" />
+      <img className='w-25' src={aboutImg} alt="תמונת המסעדה" />
+    </div>
+    <div className='text-end mt-5'>{moreAbout.map(p => (<p key={v4()}>{p}</p>))}</div>
     </div>
     </>
   )
