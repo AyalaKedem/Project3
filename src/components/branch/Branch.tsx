af87e6aa55a3dbe17e45ef52f49b10322d020e7e
AyalaKedem <ayala.bl28@gmail.com>
2023-03-06 12:18:10 +0200

    responsiveness for branches

--- src/components/branch/Branch.tsx
+++ src/components/branch/Branch.tsx
@@ -3,20 +3,28 @@ import css from "./Branch.module.scss";
 
 const Branch = ({ city, street, tel, opening, events }: BranchProp) => {
   return (
-    <div className={`${css.branch} text-end`}>
+    <div className={`${css.branch} text-sm-end`}>
       <h3 className={`${css.orange} display-6`}>{city}</h3>
-      <span className={`ms-2 ${css.fz}`}>{street} </span>
-      <span className={`fw-bold ${css.fz}`}> טל: </span>
-      <span className={css.fz}>{tel}</span>
-      <div className="d-flex flex-column flex-md-row gap-3 mt-2">
-        <h5>שעות פתיחה:</h5>
-        <div className="text-end">
-          <span className="fw-bold">א'-ה': </span>
-          <span className="ms-3">{opening.all}</span>
-          <span className="fw-bold">ו': </span>
-          <span className="ms-3">{opening.fri}</span>
-          <span className="fw-bold">שבת: </span>
-          <span>{opening.sut}</span>
+      <p className={`ms-2 mt-3 ${css.fz}`}>{street} </p>
+      <div className="d-flex gap-2">
+        <p className={`fw-bold ${css.fz}`}> טל: </p>
+        <p className={css.fz}>{tel}</p>
+      </div>
+      <div className="d-flex flex-column flex-md-row gap-3">
+        <h5 className="fw-bold text-decoration-underline">שעות פתיחה:</h5>
+        <div className="text-sm-end">
+          <div className="d-flex gap-2 text-end">
+            <p className="fw-bold">א' - ה': </p>
+            <p className="ms-3">{opening.all}</p>
+          </div>
+          <div className="d-flex gap-2 text-end">
+            <p className="fw-bold">ו': </p>
+            <p className="ms-3">{opening.fri}</p>
+          </div>
+          <div className="d-flex gap-2 text-end">
+            <p className="fw-bold">שבת: </p>
+            <p>{opening.sut}</p>
+          </div>
         </div>
       </div>
       <p className="fw-bolder">{events}</p>
