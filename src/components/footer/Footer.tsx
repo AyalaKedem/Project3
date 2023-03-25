90a345d77dc27b3c9fe524fa340418840a8fa577
AyalaKedem <ayala.bl28@gmail.com>
2023-03-26 02:18:13 +0300

    fix link for linkedin in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,13 +2,14 @@ import SocialNetBtns from "../social-net-btns/SocialNetBtns";
 import css from "./Footer.module.scss";
 
 const Footer = () => {
+  const linkedin = 'https://www.linkedin.com/in/ayala-kedem-fullstack-developer'
   return (
     <>
       <div className={css.footer}>
         <SocialNetBtns />
           {/* <div className={`bg-black ${css.lh}`}>{`© Ayala Kedem - ${new Date().getFullYear()}`}</div> */}
           <div className={`bg-black ${css.name}`}>
-            <a target={"_blank"} rel="_blank noreferrer" href={"www.linkedin.com/in/ayala-kedem-fullstack-developer"}>{`Ayala Kedem - ${new Date().getFullYear()} ©`}</a>
+            <a target={"_blank"} rel="_blank noreferrer" href={linkedin}>{`Ayala Kedem - ${new Date().getFullYear()} ©`}</a>
           </div>
           
       </div>
